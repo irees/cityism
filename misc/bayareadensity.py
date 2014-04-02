8506e894bf955ee75f405addc19bb8c4dda93114
Ian <ian@ianrees.net>
2014-04-02 10:44:19 -0700

    County profile
    Added more filters and options

--- misc/bayareadensity.py
+++ misc/bayareadensity.py
@@ -1,3 +1,4 @@
+import csv
 import numpy
 import cityism.acs
 from hist import *
@@ -14,14 +15,22 @@ from hist import *
 # Solano = 095
 # Sonoma = 097
 
-counties = ['001', '013', '075', '081', '055', '041', '085', '087', '095', '097']
+BAY_AREA_COUNTIES = ['001', '013', '075', '081', '055', '041', '085', '087', '095', '097']
+# Los Angeles counties
+LA_COUNTIES = ['037']
 kmsm = 0.386102
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--key", help="Histogram column", default='hdi')
     parser.add_argument("--metric", help="Bin metric", default='pop')
+    parser.add_argument("--bins", help="Bins", default=10, type=int)
+    parser.add_argument("--county", help="County. Can be specified multiple times. Default is 9 Bay Area Counties", action='append')
+    parser.add_argument("outfile", help="CSV output file", nargs='?')
     args = parser.parse_args()    
+
+    counties = args.county or BAY_AREA_COUNTIES
+    print "COUNTIES?", counties
 
     items = []
     with cityism.config.connect() as conn:
@@ -35,20 +44,22 @@ if __name__ == "__main__":
                 row['aland'] = row['aland'] / 1e6 * kmsm
                 row['density'] = (row['pop'] / row['aland']) 
                 items.append(row)                
-
-    for countyfp in counties:
-        c = cityism.acs.ACSFips.ANSI_STATES_COUNTIES.get(('06', countyfp))
-        citems = filter(lambda x:x.get('countyfp')==countyfp, items)
-
-        total_pop = sum(i.get('pop') for i in citems)
-        total_aland = sum(i.get('aland') for i in citems) 
-        
-
-        print "\n======== County:", countyfp, c
-        print "Population:", total_pop
-        print "Land area:", total_aland
-        print "Overall density:", (total_pop / total_aland)
-        bins = breaks(citems, key=args.key, metric=args.metric)
-        for i in bins: 
-            print i
-        
+    
+    if args.outfile:
+        with open(args.outfile, 'wb') as csvfile:
+            for countyfp in counties:
+                citems = filter(lambda x:x.get('countyfp')==countyfp, items)
+                bins = breaks(citems, key=args.key, metric=args.metric, count=args.bins)
+                total_pop = sum(i.get('pop') for i in citems)
+                total_aland = sum(i.get('aland') for i in citems) 
+                c = cityism.acs.ACSFips.ANSI_STATES_COUNTIES.get(('06', countyfp))
+                writer.writerow(["County:", countyfp, c])
+                writer.writerow(["Population:", int(total_pop)])
+                writer.writerow(["Land area (sq. mi):", '%0.2f'%total_aland])
+                writer.writerow(["Overall density:", int(total_pop/total_aland)])
+                writer.writerow(["decile", "density_min", "density_max", "pop", "aland", "# tracts", "tracts"])
+                for bin in bins:
+                    aland = sum(i.get('aland') for i in bin[5])
+                    tracts = ", ".join(i.get('name') for i in bin[5])
+                    writer.writerow([bin[0], int(bin[1]), int(bin[2]), int(bin[3]), '%0.2f'%aland, len(bin[5]), tracts])
+                writer.writerow([])
