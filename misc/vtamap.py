1f3c8d4aa17e320c5e0cd0fe60f2a1017a56027e
Ian Rees <ian@ianrees.net>
2014-06-02 04:40:21 -0700

    Bug fixes

--- misc/vtamap.py
+++ misc/vtamap.py
@@ -42,6 +42,9 @@ class Trip(object):
   def end_time(self):  
     return self.stops[-1].arrival_time.val
   
+  def duration(self):
+    return self.end_time() - self.start_time()
+  
   def as_geo(self, sched=None, **kwargs):
     f = GeoFeature(headsign=self.trip_headsign, **kwargs)
     # Check if we have shapes.txt...
@@ -97,7 +100,7 @@ class GeoFeature(object):
 ##### Main #####
 
 def headways(route, c=None, sched=None):
-  print "===== Route: %s ====="%(route)
+  print "\n===== Route: %s ====="%(route)
   c = c or GeoFeatureCollection()
   r = Route(route_id=route.route_id, route=route)
 
@@ -113,25 +116,36 @@ def headways(route, c=None, sched=None):
 
   # Calculate frequencies and build the route shape.
   for headsign,trips in r.by_headsign().items():
+    trips = sorted(trips, key=lambda x:x.start_time())
     times = [trip.start_time() for trip in trips]
+    endofday = (24*60*60) - times[-1] + times[0]
+    durations = [trip.duration() for trip in trips]
     headways = [times[i+1] - times[i] for i in range(len(times)-1)]
-    headways = [i for i in headways if i > 0]
-    print headsign, times
+    headways.append(endofday)
+    
+    print "\nHeadsign: %s"%headsign
+    for i,j,k in zip(times, durations, headways):
+      print "\tStart: %s, duration: %s, next bus in: %s"%(i,j,k)
+    
+    print "Vehicles per hour:"
+    bins = [0]*24
+    for i in times:
+      # Midnight case.
+      if i >= (24*60*60):
+        i = 0
+      bins[i/(60*60)] += 1
+    for i,j in enumerate(bins):
+      print "\t%s\t%s"%(i,j)
 
-    # We need at least 3 trips to calculate average headways...
-    if len(trips) < 3:
-      continue
+    print "Headways:"
+    for i in [0, 25, 50, 75, 100]:
+      print "\t%s:\t%s"%(i,numpy.percentile(headways, i))
 
-    # try:
-    #   headway_min = numpy.percentile(headways, 25)
-    # except Exception, e:
-    #   print "numpy:", e, headways
-    #   # continue
-    # print "Min:", headway_min
-    headway_min = min(headways)
     kwargs = {
       'headways': headways,
-      'headway_min': headway_min,
+      'headway_min': min(headways),
+      'headway_max': max(headways),
+      'headway_median': numpy.percentile(headways, 50),
       'trips': times,
       'trip_count': len(trips)
     }
@@ -144,7 +158,8 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("filename", help="GTFS .zip file, or cached sqlite DB.")
   parser.add_argument("outfile", help="GeoJSON output file.")
-  parser.add_argument("--routes", help="Run for route; helpful for debugging.", action="append")
+  parser.add_argument("--route", help="Run for route; helpful for debugging.", action="append")
+  parser.add_argument("--exclude", help="Exclude routes", action="append")
   args = parser.parse_args()
   filename = args.filename
   outfile = args.outfile
@@ -157,8 +172,10 @@ if __name__ == "__main__":
 
   # Get routes
   routes = sched.routes
-  if args.routes:
-    routes = [i for i in sched.routes if i.route_id in args.routes]
+  if args.route:
+    routes = [i for i in sched.routes if i.route_id in args.route]
+  if args.exclude:
+    routes = [i for i in sched.routes if i.route_id not in args.exclude]
 
   # Create the collection, then go through the routes
   #   and calculate the headways and route shapes.
