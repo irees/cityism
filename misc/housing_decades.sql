22b3d688cadbee2571311aa28dfe3792fed4f9f0
Ian Rees <ian@ianrees.net>
2014-06-04 00:24:08 -0700

    Renamed tables

--- misc/housing_decades.sql
+++ misc/housing_decades.sql
@@ -1,8 +1,9 @@
+-- Calculate housing unit densities for the various columns in ACS B25034.
 (SELECT 
-  tract_2012.gid AS gid,
-	tract_2012.geom AS geom,  
-	tract_2012.geoid, 
-	tract_2012.aland, 
+  tract.gid AS gid,
+	tract.geom AS geom,  
+	tract.geoid, 
+	tract.aland, 
 	acs_b25034.b25034_003/tract_2012.aland * 1e6 as density_2000_2010,
 	acs_b25034.b25034_004/tract_2012.aland * 1e6 as density_1990_2000,
 	acs_b25034.b25034_005/tract_2012.aland * 1e6 as density_1980_1990,
@@ -12,9 +13,9 @@
 	acs_b25034.b25034_009/tract_2012.aland * 1e6 as density_1940_1950,
 	acs_b25034.b25034_010/tract_2012.aland * 1e6 as density_0000_1939
 FROM 
-	tract_2012 
+	tract_2012 as tract
 INNER JOIN 
-	acs_b25034 on acs_b25034.geoid = tract_2012.geoid 
+	acs_b25034 on acs_b25034.geoid = tract.geoid 
 WHERE 
 	tract_2012.aland > 0
 ) as layer
