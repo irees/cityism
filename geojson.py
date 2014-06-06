3de1b22c3778e1bc315d3b8a72cb394b1c4289a3
Ian Rees <ian@ianrees.net>
2014-06-05 23:09:07 -0700

    Moved into new module

---
+++ geojson.py
@@ -0,0 +1,43 @@
+import json
+
+class GeoFeatureCollection(object):
+  """A geojson FeatureCollection."""
+  def __init__(self):
+    self.collection = {
+      "type": "FeatureCollection",
+      "features": []
+    }
+
+  def addfeature(self, g):
+    self.collection['features'].append(g.feature)
+  
+  def data(self):
+    return self.collection
+
+  def dump(self):
+    return json.dumps(self.data())
+
+class GeoFeature(object):
+  """A geojson Feature."""
+  def __init__(self, typegeom="LineString", coords=None, **kwargs):
+    self.feature = {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": typegeom,
+        "coordinates": coords or []
+      }
+    }
+    self.feature['properties'].update(kwargs)
+  
+  def setcoords(self, coords):
+    self.feature['geometry']['coordinates'] = coords
+
+  def getcoords(self):
+    return self.feature['geometry']['coordinates']
+  
+  def addpoint(self, lon, lat):
+    self.feature['geometry']['coordinates'].append((lon, lat))
+    
+  def data(self):
+    return self.feature
